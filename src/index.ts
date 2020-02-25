a49a9136b6e1c1492eef91a5466053cd8a1e58ce
Santiago G. Marín <santiago.marin@dotstudio.io>
2020-02-25 12:53:54 -0300

    Updated implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,237 @@
-export default true;
+import { createConnection, Connection, ConnectionOptions } from 'mongoose';
+
+export interface DatabaseClientConfig {
+  /**
+   * Mongoose connection options.
+   *
+   * @see https://mongoosejs.com/docs/connections.html#options
+   */
+  options: ConnectionOptions;
+
+  /**
+   * The connection URI.
+   */
+  uri: string;
+}
+
+export interface DatabaseClient extends DatabaseClientConfig {
+  /**
+   * Mongoose connection object.
+   */
+  connection: Connection;
+}
+
+export type DatabaseClientsMap = Map<string, DatabaseClient>;
+
+const _defaults: DatabaseClientConfig = Object.freeze<DatabaseClientConfig>({
+  uri: null,
+  options: {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  }
+});
+
+export interface DatabaseManager {
+  /**
+   * Gets a database client by name.
+   *
+   * @returns {Map} The database clients map.
+   */
+  clients: DatabaseClientsMap;
+
+  /**
+   * Retrieves a client's connection.
+   *
+   * @param {string} name The client name.
+   *
+   * @returns {Connection|null} The client's connection if any.
+   */
+  connection(name: string): Connection;
+
+  /**
+   * Adds a named database client with the provided config.
+   *
+   * @param {string} name The client name.
+   * @param {object} config The client config.
+   *
+   * @returns {object} The created client.
+   */
+  add(name: string, config: DatabaseClientConfig): void;
+
+  /**
+   * Connects a named client to the database.
+   *
+   * @param {string} name The client name.
+   *
+   * @returns {Connection} The current client connection.
+   */
+  connect(name: string): Promise<Connection>;
+
+  /**
+   * Disconnects a named client from the database.
+   *
+   * @param {string} name The client name.
+   * @param {boolean} force Whether to force disconnection.
+   *
+   * @returns {Promise} A promise to the disconnection.
+   */
+  disconnect(name: string, force?: boolean): Promise<void>;
+
+  /**
+   * Disconnects all clients from their databases.
+   *
+   * @param {boolean} force Whether to force disconnection.
+   */
+  disconnectAll(force?: boolean): Promise<void>;
+}
+
+/**
+ * Retrieves a connection by client name.
+ *
+ * @param {Map} clients The clients map.
+ * @param {string} name The client name.
+ * @returns {object} The mongoose connection object.
+ */
+function getConnectionByName(clients: DatabaseClientsMap, name: string): Connection {
+  if (clients.has(name)) {
+    return clients.get(name).connection;
+  }
+
+  return null;
+}
+
+/**
+ * Creates or updates a named database client.
+ *
+ * @param {object} config The configuration object.
+ * @param {object} connection The current database client connection.
+ *
+ * @returns {object} The created or updated database connection object.
+ */
+function create(config: DatabaseClientConfig, connection: Connection): DatabaseClient {
+  const { options, uri }: DatabaseClientConfig = {
+    ..._defaults,
+    ...config,
+    options: {
+      ..._defaults.options,
+      ...config.options
+    }
+  };
+
+  const client: DatabaseClient = { options, connection, uri };
+
+  return client;
+}
+
+/**
+ * Returns the connection for the client.
+ *
+ * @param {Map} clients The clients map.
+ * @param {string} name The client name.
+ * @returns {Connection} The client connection.
+ */
+function connection(clients: DatabaseClientsMap, name: string): Connection {
+  const { connection } = clients.get(name);
+
+  return connection;
+}
+
+/**
+ * Creates a client's database connection or reuses it.
+ *
+ * @param {string} name The client name.
+ * @param {object} client The database client object.
+ *
+ * @returns {Connection} The current or created connection.
+ */
+async function connect(name, { connection, uri, options }: DatabaseClient): Promise<Connection> {
+  if (connection) {
+    return connection;
+  }
+
+  return createConnection(uri, options);
+}
+
+/**
+ * Closes a database connection by name.
+ *
+ * @param {Map} client The client to disconnect.
+ * @param {boolean} force Whether to force disconnection.
+ *
+ * @returns {object} The updated database client.
+ */
+function disconnect(client: DatabaseClient, force = false): Promise<void> {
+  if (!client.connection) {
+    return Promise.resolve();
+  }
+
+  return client.connection.close(force);
+}
+
+/**
+ * Updates a database client connection.
+ *
+ * @param {Map} clients The database clients map.
+ * @param {string} name The database client name.
+ * @param {Connection} connection The connection to set.
+ */
+function updateClientConnection(clients: DatabaseClientsMap, name: string, connection: Connection): void {
+  const client = clients.get(name);
+
+  clients.set(name, {
+    ...client,
+    connection
+  });
+}
+
+/**
+ * Creates a database manager instance.
+ *
+ * @returns {object} The database manager instance.
+ */
+export function createDatabaseManager(): DatabaseManager {
+  const clients = new Map<string, DatabaseClient>();
+
+  return Object.freeze<DatabaseManager>({
+    connection: connection.bind(null, clients),
+
+    get clients(): DatabaseClientsMap {
+      return clients;
+    },
+
+    add: (name, config) => {
+      const connection = getConnectionByName(clients, name);
+      const client = create(config, connection);
+
+      clients.set(name, client);
+
+      return client;
+    },
+
+    connect: async name => {
+      const client: DatabaseClient = clients.get(name);
+      const connection: Connection = await connect(name, client);
+
+      updateClientConnection(clients, name, connection);
+
+      return connection;
+    },
+
+    disconnect: async (name, force) => {
+      const client: DatabaseClient = clients.get(name);
+
+      await disconnect(client, force);
+
+      updateClientConnection(clients, name, null);
+    },
+
+    disconnectAll: async force => {
+      for (const [name, client] of clients.entries()) {
+        await disconnect(client, force);
+
+        updateClientConnection(clients, name, null);
+      }
+    }
+  });
+}
--- test/src/index.spec.ts
+++ test/src/index.spec.ts
@@ -1,9 +1,216 @@
+import { MongoMemoryServer } from 'mongodb-memory-server-core';
+import mongoose from 'mongoose';
 import { expect } from 'chai';
 
-import index from '../../src';
+import { createDatabaseManager, DatabaseClientConfig, DatabaseManager } from '../../src';
 
-describe('Index', function () {
-  it('should be true', function () {
-    expect(index).to.be.true;
+const servers: Map<string, MongoMemoryServer> = new Map();
+
+servers.set('primary', new MongoMemoryServer());
+servers.set('secondary', new MongoMemoryServer());
+servers.set('tertiary', new MongoMemoryServer());
+
+const methods = [
+  'connect', 'connection', 'disconnect', 'disconnectAll'
+];
+
+describe('Database', function () {
+  this.timeout(30000);
+
+  let db: DatabaseManager;
+
+  const config: DatabaseClientConfig = {
+    options: {},
+    uri: null
+  };
+
+  before(function () {
+    db = createDatabaseManager();
+
+    db.add('default', { ...config });
+  });
+
+  it('should be a function', function () {
+    expect(createDatabaseManager).to.be.a('function');
+  });
+
+  it('should expose API methods', function () {
+    for (const method of methods) {
+      expect(db[String(method)]).to.be.a('function');
+    }
+  });
+
+  it('should expose Map API methods', function () {
+    const map = new Map();
+
+    for (const method of Object.keys(map)) {
+      expect(db[String(method)]).to.be.a('function');
+    }
+  });
+
+  it('should not set a connection with empty config', function () {
+    expect(() => db.add(null, null)).to.throw();
+    expect(() => db.add(undefined, undefined)).to.throw();
+    expect(() => db.add(null, undefined)).to.throw();
+    expect(() => db.add(undefined, null)).to.throw();
+  });
+
+  it('should create a new connection with a valid config as default', function () {
+    db.add('default', config);
+
+    const keys = [...db.clients.keys()];
+
+    expect(keys).to.not.be.empty;
+    expect(keys).to.be.an('array');
+    expect(keys).to.include('default');
+
+    const client = db.clients.get('default');
+
+    expect(client).to.be.an('object');
+    expect(client).to.have.keys('options', 'uri', 'connection');
+
+    expect(client.options).to.be.an('object');
+    expect(client.uri).to.be.null;
+    expect(client.connection).to.be.null;
+  });
+
+  it('should update a connection with a valid config as default', async function () {
+    const uri = await servers.get('primary').getConnectionString();
+
+    db.add('default', {
+      ...config,
+      uri
+    });
+
+    const keys = [...db.clients.keys()];
+
+    expect(keys).to.not.be.empty;
+    expect(keys).to.be.an('array');
+    expect(keys).to.include('default');
+
+    const client = db.clients.get('default');
+
+    expect(client).to.be.an('object');
+    expect(client).to.have.keys('options', 'uri', 'connection');
+
+    expect(client.options).to.be.an('object');
+    expect(client.uri).to.be.a('string');
+    expect(client.uri).to.equal(uri);
+    expect(client.connection).to.be.null;
+  });
+
+  it('should have a default connection', function () {
+    const hasClient = db.clients.has('default');
+
+    expect(hasClient).to.be.true;
+  });
+
+  it('should not be connected to a database as default', function () {
+    const client = db.clients.get('default');
+
+    expect(client.connection).to.be.null;
+  });
+
+  it('should connect to a database as default', async function () {
+    const conn = await db.connect('default');
+
+    expect(conn).to.be.an('object');
+    expect(conn).to.be.an.instanceof(mongoose.Connection);
+    expect(conn.readyState).to.equal(1);
+  });
+
+  it('should reuse the database connection', async function () {
+    const { connection } = db.clients.get('default');
+
+    for (let i = 0, l = 3; i < l; i++) {
+      expect(await db.connect('default')).to.deep.equal(connection);
+    }
+  });
+
+  it('should register a model', function () {
+    const conn = db.connection('default');
+
+    const Person = conn.model('person', new mongoose.Schema({
+      name: String
+    }));
+
+    expect(Person).to.be.a('function');
+    expect(Person).to.have.property('name');
+  });
+
+  it('should retrieve a model', async function () {
+    const Person = db.clients.get('default').connection.model('person');
+
+    expect(Person).to.be.a('function');
+    expect(Person).to.have.property('name');
+  });
+
+  it('should handle multiple named connections', async function () {
+    for (const name of servers.keys()) {
+      db.add(name, {
+        ...config,
+        uri: await servers.get(name).getConnectionString()
+      });
+
+      expect(db.clients.has(name)).to.be.true;
+      expect(db.clients.get(name)).to.be.an('object');
+      expect(db.clients.get(name).connection).to.be.null;
+
+      const conn = await db.connect(name);
+
+      expect(db.clients.get(name).connection).to.be.deep.equal(conn);
+      expect(conn).to.be.an('object');
+      expect(conn).to.be.an.instanceof(mongoose.Connection);
+      expect(conn.readyState).to.equal(1);
+    }
+
+    // Check uniqueness
+    for (const srv of servers.keys()) {
+      for (const cnn of db.clients.keys()) {
+        if (srv !== cnn) {
+          expect(db.clients.get(cnn)).to.not.be.deep.equal(db.clients.get(srv));
+        }
+      }
+    }
+  });
+
+  it('should list all keys', function () {
+    const keys = [...db.clients.keys()];
+
+    expect(keys).to.be.an('array');
+    expect(keys).to.have.length.of.at.least(1);
+  });
+
+  it('should list all entries', function () {
+    const entries = [...db.clients.entries()];
+
+    expect(entries).to.be.an('array');
+    expect(entries).to.have.length.of.at.least(1);
+  });
+
+  it('should list all values', function () {
+    const clients = [...db.clients.values()];
+
+    expect(clients).to.be.an('array');
+    expect(clients).to.have.length.of.at.least(1);
+  });
+
+  it('should disconnect the default from the database', async function () {
+    await db.disconnect('default');
+
+    expect(db.connection('default')).to.be.null;
+  });
+
+  it('should disconnect all from the database', async function () {
+    await db.disconnectAll();
+  });
+
+  after(async function () {
+    await mongoose.disconnect();
+    await db.disconnectAll(true);
+
+    for (const server of servers.keys()) {
+      await servers.get(server).stop();
+    }
   });
 });
